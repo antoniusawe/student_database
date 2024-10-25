ef0905fb67ddc9061210ac57c5baee723f5099d6
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-25 15:22:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Sidebar kosong
-st.sidebar.title("Sidebar")  # Ini adalah judul sidebar yang dapat Anda sesuaikan
+st.sidebar.title("RYP")  # Ini adalah judul sidebar yang dapat Anda sesuaikan
 
 # Halaman utama
 st.title("2025 RYP Student Database")
