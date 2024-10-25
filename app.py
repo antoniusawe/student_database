19bb6d87ff488409f46e48e4870520c06c36b09a
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-25 15:02:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-# URL dari file Excel di GitHub
-file_url = 'https://raw.githubusercontent.com/antoniusawe/student_database/main/student_database_200hr.xlsx'
+# Periksa query parameter untuk menentukan halaman yang ditampilkan
+query_params = st.experimental_get_query_params()
+page = query_params.get("page", ["home"])[0]
 
-st.title("Student Database RYP")
+# Cek apakah halaman saat ini adalah "home"
+if page == "home":
+    st.write("Anda berada di halaman utama. Silakan kembali ke home.py.")
+else:
+    # URL dari file Excel di GitHub
+    file_url = 'https://raw.githubusercontent.com/antoniusawe/student_database/main/student_database_200hr.xlsx'
 
-# Membaca file Excel dari URL
-try:
-    data = pd.read_excel(file_url)
+    st.title("Student Database Viewer")
 
-    # Tampilkan data di Streamlit
-    st.write("## Student Database 200HR")
-    st.dataframe(data)
+    # Membaca file Excel dari URL
+    try:
+        data = pd.read_excel(file_url)
 
-except Exception as e:
-    st.error(f"Terjadi kesalahan saat membaca file: {e}")
+        # Tampilkan data di Streamlit
+        st.write("## Student Database")
+        st.dataframe(data)
+
+    except Exception as e:
+        st.error(f"Terjadi kesalahan saat membaca file: {e}")
