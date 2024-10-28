4edb65934dae5c19e32def592d0f03e96afc1aaf
antoniusawe <95326273+antoniusawe@users.noreply.github.com>
2024-10-28 11:28:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,10 @@ if generate_button:
 
         # Menampilkan data dalam bentuk tabel
         st.dataframe(df_200hr_stud)
-                
+        
+        df_200hr_stud['Batch start date'] = pd.to_datetime(df_200hr_stud['Batch start date'], format='%B %d, %Y')
+        df_200hr_stud['Batch end date'] = pd.to_datetime(df_200hr_stud['Batch end date'], format='%B %d, %Y')
+        
         # Grouping the data
         batch_booking_source_200hr = df_200hr_stud.groupby(
             ['Batch start date', 'Batch end date', 'Booking source']
